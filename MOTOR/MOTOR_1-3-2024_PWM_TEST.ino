5fac5a9de80325b52e59260998b9bb4972d61c67
bjoron <161816836+bjoron@users.noreply.github.com>
2024-03-03 11:17:38 +0100

    Update MOTOR_2-3-2024_PWM_TEST.ino

--- MOTOR/MOTOR_1-3-2024_PWM_TEST.ino
+++ MOTOR/MOTOR_1-3-2024_PWM_TEST.ino
@@ -4,13 +4,13 @@ void setup() {
   TCCR2B = 0b00000001;
   /*Sätter D11 till motorpin*/
   DDRB = 0b00001000;
-  /*Hastighet för motor med lägre gräns = 50. 
+  /*Hastighet för motor med lägre gräns = 70. 
   Vid "kallstart" fungerar ej låga hastigheter 
-  (<~90), då kan max-värde (255) köras i 
+  (<~100), då kan max-värde (255) köras i 
   20 ms innan den låga hastigheten sätts*/
   OCR2A = 255;
   delay(20);
-  OCR2A = 50;
+  OCR2A = 70;
 }
 
 void loop() {
