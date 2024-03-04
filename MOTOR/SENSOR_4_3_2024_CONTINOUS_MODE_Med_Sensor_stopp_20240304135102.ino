43cb767e73596d52c89fabc1a1cc2dad3c884281
LucasCurry <123386797+LucasCurry@users.noreply.github.com>
2024-03-04 13:54:07 +0100

    Add files via upload

---
+++ MOTOR/SENSOR_4_3_2024_CONTINOUS_MODE_Med_Sensor_stopp_20240304135102.ino
@@ -0,0 +1,87 @@
+// This example shows how to use non-blocking code to read
+// from all three channels on the OPT3101 continuously
+// and store the results in arrays.
+//
+// In addition to the usual power and I2C connections, you
+// will need to connect the GP1 pin to an interrupt on your
+// Arduino. Use pin 2. To use a different pin, change the
+// definition of dataReadyPin below, and refer to the
+// documentation of attachInterrupt to make sure you
+// pick a valid pin.
+//
+// "Advanced"-koden ger 1 sampling / 40 ms
+// Den här koden ger 1 sampling / 8,4 ms
+
+#include <OPT3101.h>
+#include <Wire.h>
+
+const uint8_t dataReadyPin = 2;
+
+OPT3101 sensor;
+
+int16_t distances[3];
+volatile bool dataReady = false;
+
+void setDataReadyFlag()
+{
+  dataReady = true;
+}
+
+void setup()
+{
+    /*PWM-inställning*/
+  TCCR2A = 0b10000011;
+  TCCR2B = 0b00000001;
+  /*Sätter D11 (Atmegas PB3) till motorpin*/
+  DDRB = 0b00001000;
+  /*Hastighet för motor med lägre gräns = 70. 
+  Vid "kallstart" fungerar ej låga hastigheter 
+  (<~100), då kan max-värde (255) köras i 
+  20 ms innan den låga hastigheten sätts*/
+  OCR2A = 255;
+  delay(20);
+  OCR2A = 140;
+
+  Serial.begin(9600);
+  Wire.begin();
+  sensor.init();
+  if (sensor.getLastError())
+  {
+    Serial.print(F("Failed to initialize OPT3101: error "));
+    Serial.println(sensor.getLastError());
+    while (1) {}
+  }
+  sensor.setContinuousMode();
+  sensor.enableDataReadyOutput(1);
+  sensor.setFrameTiming(32);
+  sensor.setChannel(OPT3101ChannelAutoSwitch);
+  sensor.setBrightness(OPT3101Brightness::Adaptive);
+  attachInterrupt(digitalPinToInterrupt(dataReadyPin), setDataReadyFlag, RISING);
+  sensor.enableTimingGenerator();
+}
+
+void loop()
+{
+  if (dataReady)
+  {
+    sensor.readOutputRegs();
+    distances[sensor.channelUsed] = sensor.distanceMillimeters;
+    if (sensor.channelUsed == 2)
+    {
+      for (uint8_t i = 0; i < 3; i++)
+      {
+        Serial.print(distances[i]);
+        Serial.print(", ");
+      }
+      Serial.println();
+    }
+    dataReady = false;
+  }
+  if (sensor.channelUsed == 1 && sensor.distanceMillimeters < 200) {
+      OCR2A = 0;
+  } else if (sensor.channelUsed == 1 && sensor.distanceMillimeters > 240) {
+    OCR2A = 255;
+    delay(20);
+    OCR2A = 70;
+  }
+}
